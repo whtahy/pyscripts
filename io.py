ee54258d829b6a56c079297dec0ac9c55e2d78dc
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-08-19 03:20:46 -0400

    Fix/cleanup filename arg processing

--- io.py
+++ io.py
@@ -1,8 +1,11 @@
 import pickle
+import string
 
 from pandas import read_csv
 from inspect import getsourcefile
 from os.path import abspath, dirname, basename, join
+
+from .misc import strip_ext
 
 
 def file_path(local_object):
@@ -22,25 +25,21 @@ def read_file(*path, encoding='utf-8'):
         return list(file.read().replace('\n', ''))
 
 
-def make_paths(file_names, path='', ext=''):
-    if ext:
-        return [f'{path}/{name}.{ext}' for name in file_names]
-    else:
-        return [f'{path}/{name}' for name in file_names]
-
-
-def pickle_obj(obj, file_path, ext='pickle'):
-    with open(f'{file_path}.{ext}', 'wb') as file:
+def pickle_obj(obj, save_path, ext='pickle'):
+    save_path = strip_ext(save_path, ext)
+    with open(f'{save_path}.{ext}', 'wb') as file:
         pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
 
 
 def unpickle_file(file_path, ext='pickle'):
+    file_path = strip_ext(file_path, ext)
     with open(f'{file_path}.{ext}', 'rb') as file:
         data = pickle.load(file)
     return data
 
 
-def pickle_csv(file_path,
+def pickle_csv(csv_path,
+               save_path,
                ext='pickle',
                nrows=None,
                usecols=None,
@@ -50,10 +49,11 @@ def pickle_csv(file_path,
     '''
     Use pandas to preserve dtypes!
     '''
-    csv_data = read_csv(f'{file_path}.csv',
+    csv_path = strip_ext(csv_path, 'csv')
+    csv_data = read_csv(f'{csv_path}.csv',
                         nrows=nrows,
                         usecols=usecols,
                         index_col=index_col,
                         dtype=dtype_dict,
                         header=header)
-    pickle_obj(csv_data, file_path)
+    pickle_obj(csv_data, save_path)
