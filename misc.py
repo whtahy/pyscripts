bf1b19a3c140fe923f4c34737e3973e08363263e
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-10-28 18:41:40 -0400

    Refactor: use ndarray > subset_apply @ misc.py

--- misc.py
+++ misc.py
@@ -6,6 +6,9 @@
 import itertools
 from typing import TYPE_CHECKING
 
+import numpy
+from scipy.misc import comb
+
 if TYPE_CHECKING:
     from typing import *
     from pyscripts.types import *
@@ -13,14 +16,21 @@ if TYPE_CHECKING:
     T = TypeVar('T')
 
 
+def max_length(
+        arr: 'Iterable') \
+        -> int:
+    return max([len(str(x)) for x in arr])
+
+
 def subset_apply(
-        objects: 'Iterable[Any]',
-        func: 'Callable[..., T]',
+        objects: 'Iterable',
+        func: 'Callable',
         l_subset: int = 2) \
-        -> 'List[T]':
-    out = []
-    for subset in itertools.combinations(objects, l_subset):
-        out += [func(*subset)]
+        -> 'ndarray':
+    out = numpy.empty(comb(len(objects), l_subset).astype('int'),
+                      dtype = 'object')
+    for i, subset in enumerate(itertools.combinations(objects, l_subset)):
+        out[i] = func(*subset)
     return out
 
 
