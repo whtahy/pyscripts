a3ab4dacb8028654a7b45cc34bb21c26173b3d95
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-11-05 13:36:56 -0500

    Refactor+rename: re_repo -> re_github @hero.py

--- pyscripts/hero.py
+++ pyscripts/hero.py
@@ -8,7 +8,7 @@ import re
 
 re_ext = re.compile(r'.+(\.\w+)$')
 re_imports = re.compile(r'^\s*(from|import)\s+\w+')
-re_repo = re.compile(r'github\.com/[\w-]+/[\w-]+')
+re_github = re.compile(r'[htps:/]+github\.com/[\w-]+/([\w-]+)')
 re_slash = re.compile(r'[\\/]+')
 re_startword = re.compile(r'^\w+')
 re_typename = re.compile(r'([a-zA-Z]+)(\d*).*$')
