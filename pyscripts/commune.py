50d372fd501d1aac11255ee10d95c752ef66878e
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-11-06 22:24:33 -0500

    Adjust: PYPLOT_HEIGHT 17 -> 16 @commune.py

--- pyscripts/commune.py
+++ pyscripts/commune.py
@@ -16,9 +16,8 @@ SEED: int = 123
 # 1680 x 1050 resolution
 TERM_WIDTH = 180
 PYPLOT_WIDTH: int = 26
-PYPLOT_HEIGHT: int = 17
+PYPLOT_HEIGHT: int = 16
 
 # Package info
 PROJECT_NAME = 'pyscripts'
 LICENSE_DIR = 'licenses'
-
