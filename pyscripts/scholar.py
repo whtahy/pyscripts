77b14588134781fbe0b518cf800d14254471f5dc
Tae Hyon Whang <whtahy@users.noreply.github.com>
2018-02-05 18:28:08 -0500

    Feature: scan_dir + read_file; Remove: dir_apply + file_apply

--- pyscripts/scholar.py
+++ pyscripts/scholar.py
@@ -13,51 +13,9 @@ from typing import TYPE_CHECKING
 import numpy
 from pandas import read_csv
 
-from pyscripts.zfc import numpy_hstack
-
 if TYPE_CHECKING:
     from typing import *
     from pyscripts.mytypes import *
-
-
-def dir_apply(
-        start_path: str,
-        func: 'Callable[str, Any]' = lambda x: x,
-        recursive: bool = True,
-        folder_filter: 'Callable[str, bool]' = lambda x: True,
-        file_filter: 'Callable[str, bool]' = lambda x: True) \
-        -> 'ndarray':
-    out = numpy.array([], dtype = 'object')
-    for dir_name, subdir_names, file_names in os.walk(start_path):
-        if recursive:
-            subdir_names[:] = [name for name in subdir_names if
-                               folder_filter(name)]
-        else:
-            subdir_names[:] = []
-        for f in file_names:
-            if file_filter(f):
-                f_path = os.path.join(dir_name, f)
-                out = numpy_hstack(out, func(f_path))
-    return out
-
-
-def file_apply(
-        path: str,
-        func: 'Callable[str, Any]' = lambda x: x,
-        output_filter: 'Callable[Any, bool]' = lambda x: True,
-        break_condition: 'Callable[str, Any]' = lambda x: False,
-        encoding: str = 'utf-8') \
-        -> 'ndarray':
-    out = numpy.array([], dtype = 'object')
-    with open(path, encoding = encoding) as file:
-        for line in file:
-            if break_condition(line):
-                break
-            else:
-                func_out = func(line)
-                if output_filter(func_out):
-                    out = numpy_hstack(out, func(line))
-    return out
 
 
 def file_path(
@@ -80,21 +38,49 @@ def file_name(
 
 def read_file(
         path: str,
-        encoding: str = 'utf-8') \
-        -> 'ndarray':
+        encoding: str = 'utf-8',
+        line_filter: 'Callable[str, bool]' = lambda x: True,
+        break_condition: 'Callable[str, bool]' = lambda x: False) \
+        -> 'List[str]':
     out = []
     with open(path, encoding = encoding) as file:
         for line in file:
-            out += [line.replace('\n', '')]
-    return numpy.array(out, dtype = 'object')
+            if break_condition(line):
+                break
+            if line_filter(line):
+                out += [line.rstrip('\n')]
+    return out
+
+
+def scan_dir(
+        path: str,
+        folder_filter: 'Callable[str, bool]' = lambda x: True,
+        file_filter: 'Callable[str, bool]' = lambda x: True,
+        recursive: bool = True) \
+        -> 'List[str]':
+    out = []
+    for dir_name, subdir_names, file_names in os.walk(path):
+        if recursive:
+            subdir_names[:] = [name for name in subdir_names if
+                               folder_filter(name)]
+        else:
+            subdir_names[:] = []
+
+        for f in file_names:
+            if file_filter(f):
+                f_path = os.path.join(dir_name, f)
+                out += [f_path]
+    return out
 
 
 def split_file(
         path: str,
-        encoding: str = 'utf-8') \
+        encoding: str = 'utf-8',
+        chunk_size: int = 1) \
         -> 'ndarray':
     with open(path, encoding = encoding) as file:
-        return numpy.fromiter(file.read().replace('\n', ''), dtype = 'U1')
+        return numpy.fromiter(file.read().replace('\n', ''),
+                              dtype = f'U{chunk_size}')
 
 
 def pickle_obj(
